da8ed701f76507daaf054985d4e1e15521b7e58e
James McCreight <jamesmcc@ucar.edu>
2024-12-20 20:46:49 -0700

    update notebooks

--- .github/scripts/test_notebooks.py
+++ .github/scripts/test_notebooks.py
@@ -11,11 +11,11 @@ repo_dir = pl.Path("../../").resolve()
 notebooks_to_test = [
     "0a_Workspace_setup.ipynb",
     "0b_Create_poi_files.ipynb",
-    # "0c_Create_Streamflow_Observations_v3.ipynb",
-    # "1a_Framework_Inspection.ipynb",
+    # "0c_Create_Streamflow_Observations_v3.ipynb",  # broken
+    "1a_Framework_Inspection.ipynb",
     # "1b_Parameter_Visualization.ipynb",
-    # "2a_preprocessing_for_pyWatershed.ipynb",
-    # "2b_Run_NHMx_pyWatershed.ipynb",
+    "2a_preprocessing_for_pyWatershed.ipynb",
+    "2b_Run_NHMx_pyWatershed.ipynb",
     # "3a_HRU_Output_Visualization.ipynb",
     # "3b_Streamflow_Output_Visualization.ipynb",
 ]
@@ -39,7 +39,7 @@ def clean_notebook(nb_name):
         "nbconvert",
         "--ClearOutputPreprocessor.enabled=True",
         "--ClearMetadataPreprocessor.enabled=True",
-        "--ClearMetadataPreprocessor." + "preserve_nb_metadata_mask={('kernelspec')}",
+        "--ClearMetadataPreprocessor.preserve_nb_metadata_mask={('kernelspec')}",
         "--inplace",
         f"{nb_name}",
     )
