9839f88699e84803cfcc0f92211615ebf80bec86
James McCreight <jamesmcc@ucar.edu>
2024-12-21 08:32:32 -0700

    add all notebooks

--- .github/scripts/test_notebooks.py
+++ .github/scripts/test_notebooks.py
@@ -11,13 +11,13 @@ repo_dir = pl.Path("../../").resolve()
 notebooks_to_test = [
     "0a_Workspace_setup.ipynb",
     "0b_Create_poi_files.ipynb",
-    "0c_Create_Streamflow_Observations_v3.ipynb",  # broken
+    "0c_Create_Streamflow_Observations_v3.ipynb",
     "1a_Framework_Inspection.ipynb",
-    # "1b_Parameter_Visualization.ipynb",
+    "1b_Parameter_Visualization.ipynb",
     "2a_preprocessing_for_pyWatershed.ipynb",
     "2b_Run_NHMx_pyWatershed.ipynb",
-    # "3a_HRU_Output_Visualization.ipynb",
-    # "3b_Streamflow_Output_Visualization.ipynb",
+    "3a_HRU_Output_Visualization.ipynb",
+    "3b_Streamflow_Output_Visualization.ipynb",
 ]
 
 
