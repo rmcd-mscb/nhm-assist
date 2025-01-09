1488cc2eb15460f1e4aa38f731b4b8390379df6c
James McCreight <jamesmcc@ucar.edu>
2025-01-09 12:51:46 -0700

    make sure all notebooks are tested unless ones are specifically marked to not test

--- .github/scripts/test_notebooks.py
+++ .github/scripts/test_notebooks.py
@@ -8,17 +8,10 @@ from pywatershed.utils.utils import timer
 
 repo_dir = pl.Path("../../").resolve()
 
-notebooks_to_test = [
-    "0a_Workspace_setup.ipynb",
-    "0b_Create_poi_files.ipynb",
-    "0c_Create_Streamflow_Observations_v3.ipynb",
-    "1a_Framework_Inspection.ipynb",
-    "1b_Parameter_Visualization.ipynb",
-    "2a_preprocessing_for_pyWatershed.ipynb",
-    "2b_Run_NHMx_pyWatershed.ipynb",
-    "3a_HRU_Output_Visualization.ipynb",
-    "3b_Streamflow_Output_Visualization.ipynb",
-]
+all_notebooks = repo_set(dir.glob("*.ipynb"))
+# Add notebooks here as needed
+notebooks_to_not_test = set()
+notebooks_to_test = list(all_notebooks - notebooks_not_to_test)
 
 
 def run_cmd(cmd):
