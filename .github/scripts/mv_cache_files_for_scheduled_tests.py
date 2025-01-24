f6895e64d84ecd6a21f718443a27667fa46791d7
James McCreight <jamesmcc@ucar.edu>
2025-01-24 08:29:17 -0700

    caching shenanigans for ci

--- .github/scripts/mv_cache_files_for_scheduled_tests.py
+++ .github/scripts/mv_cache_files_for_scheduled_tests.py
@@ -2,38 +2,43 @@ import pathlib as pl
 import shutil
 
 repo_dir = pl.Path("../../").resolve()
-
-nc_files_path = repo_dir / "domain_data/willamette_river/notebook_output_files/nc_files"
-folium_maps_paths = (
-    repo_dir / "domain_data/willamette_river/notebook_output_files/Folium_maps"
-)
-
-files_to_rm = [
-    nc_files_path / "owrd_cache.nc",
-    nc_files_path / "sf_efc.nc",
-]
+tmp_path = repo_dir / "tmp_relocation_of_output"
+nb_output_dir = repo_dir / "domain_data/willamette_river/notebook_output_files"
+nc_files_path = nb_output_dir / "nc_files"
+paths_to_mv = {
+    "owrd_cache.nc": (nc_files_path, tmp_path),
+    "sf_efc.nc": (nc_files_path, tmp_path),
+    "Folium_maps": (nb_output_dir, tmp_path),
+}
 
 
-dirs_to_rm = [
-    folium_maps_paths,
-]
+def relocate_path(name: str, from_dir: pl.Path, to_dir: pl.Path):
+    src = from_dir / name
+    dst = to_dir / name
+    print(f"relocating {str(src)} -> {str(dst)}")
+    src.rename(dst)
+    assert dst.exists()
+    assert not src.exists()
+    return
 
 
-def del_path(path: pl.Path):
-    if path.exists():
-        print(f"deleting path: {path}")
-        path.unlink()
+def mv_cache_files_for_scheduled_tests():
+    if not tmp_path.exists():
+        tmp_path.mkdir()
+    for key, val in paths_to_mv.items():
+        relocate_path(key, val[0], val[1])
+
+    return
 
 
-def del_dir(path: pl.Path):
-    if path.exists():
-        print(f"deleting directory: {path}")
-        shutil.rmtree(path)
+def restore_cache_files_after_scheduled_tests():
+    for key, val in paths_to_mv.items():
+        relocate_path(key, val[1], val[0])
+
+    shutil.rmtree(tmp_path)
+
+    return
 
 
 if __name__ == "__main__":
-    for ff in files_to_rm:
-        del_path(ff)
-
-    for dd in dirs_to_rm:
-        del_dir(dd)
+    mv_cache_files_for_scheduled_tests()
---
+++ .github/scripts/restore_cache_files_after_scheduled_tests.py
@@ -0,0 +1,4 @@
+from mv_cache_files_for_scheduled_tests import restore_cache_files_after_scheduled_tests
+
+if __name__ == "__main__":
+    restore_cache_files_after_scheduled_tests()
